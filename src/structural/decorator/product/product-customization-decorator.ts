f048d1c9d52e2cda6fdfb555fe171321b8c0f835
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-02 22:30:47 -0300

    fix(decorator): filenames and class name

--- src/structural/decorator/product/product-customization-decorator.ts
+++ src/structural/decorator/product/product-customization-decorator.ts
@@ -1,6 +1,6 @@
 import { ProductDecorator } from './product-decorator';
 
-export class ProductCustomization extends ProductDecorator {
+export class ProductCustomizationDecorator extends ProductDecorator {
   getPrice(): number {
     return this.product.getPrice() + 10;
   }
